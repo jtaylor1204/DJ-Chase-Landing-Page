65b685c40b0e3c4c33995cbcc9a6bad037aba1d6
jtaylor1204 <jaidataylor4@gmail.com>
2022-12-31 01:19:10 -0500

    changed responsiveness

--- player.js
+++ player.js
@@ -2,20 +2,22 @@
     $(document).ready(function () {
 
         // Pause/Play functionality
-       
-        let button = $('#print');
-        let showVinyl = $('#vinyl');
-        let wrap = $('#wrap');
-        let song = new Audio ('media/audio/DJ Chase - Make A Wish.mp3');
+
+        let button = $('.print');
+        let showVinyl = $('.vinyl');
+        let song = new Audio('media/audio/DJ Chase - Make A Wish.mp3');
 
         button.on('click', function () {
             showVinyl.toggleClass("vinyl-out");
             button.toggleClass('is-playing');
             song.play();
-        if(!button.hasClass('is-playing')){
-            song.pause();
-        }
+            if (!button.hasClass('is-playing')) {
+                song.pause();
+            }
+            
         });
+
+
 
 
     })
